d630222564b48789b74e20b565647b8cedf51e5f
Carl Goldsmith <scrobby@users.noreply.github.com>
2021-12-09 11:14:42 +0000

    Changed fields to inline, added landline

--- client/src/components/generate-signature.js
+++ client/src/components/generate-signature.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 
-import { Row, Col, Form, Spinner } from 'react-bootstrap'
+import { Row, Col, Form, Spinner, FloatingLabel } from 'react-bootstrap'
 
 import { useMsal } from '@azure/msal-react'
 import { loginRequest } from '../authConfig'
@@ -13,6 +13,7 @@ export function GenerateSignature(props) {
     const [fullName, setFullName] = useState("")
     const [jobTitle, setJobTitle] = useState("")
     const [mobile, setMobile] = useState("")
+    const [landline, setLandline] = useState("01273 789090")
     const [userEmail, setUserEmail] = useState("")
     const [showLandline, setShowLandline] = useState(true)
     const [showMobile, setShowMobile] = useState(true)
@@ -29,6 +30,7 @@ export function GenerateSignature(props) {
             setFullName(profileData.displayName ? profileData.displayName : "")
             setJobTitle(profileData.jobTitle ? profileData.jobTitle : "")
             setMobile(profileData.mobilePhone ? profileData.mobilePhone : "")
+            setLandline(profileData.businessPhones ? profileData.businessPhones[0] ? profileData.businessPhones[0] : "" : "")
             setShowMobile(profileData.mobilePhone ? true : false)
             setUserEmail(profileData.mail ? profileData.mail : "")
             setIsLoading(false)
@@ -39,11 +41,11 @@ export function GenerateSignature(props) {
     let sigStart = '<p style="font-size:12px;font-family:Calibri, Arial, Sans-Serif;"><strong><span style="color: #333;">'
     let sigNameTitle = (fullName ? fullName : 'Full Name') + ' |</span>&nbsp;<span style="color: #fd6320;">' + (jobTitle ? jobTitle : 'Job Title')
     let sigAddress = '</span></strong><br /> <span style="color: #555;">Lambent Productions, 96 Church Street, Brighton, BN1 1UJ</span>'
-    let sigLandline = (showLandline ? '<br /><span style="color: #555;">T:</span>&nbsp;<a href="tel:01273789090" style="color: #1b9ce2;">01273 789090</a>' : '')
+    let sigLandline = (showLandline ? '<br /><span style="color: #555;">T:</span>&nbsp;<a href="tel:' + landline.replace(/\s/g, '') + '" style="color: #1b9ce2;">' + landline + '</a>' : '')
     let sigMobile = (mobile && showMobile ? '<br /><span style="color: #555;">M:</span>&nbsp;<a href="tel:' + mobile.replace(/\s/g, '') + '" style="color: #1b9ce2;">' + mobile + '</a>' : '')
     let sigEmail = (showEmail ? '<br /><span style="color: #555;">E:</span>&nbsp;<a href="mailto:"' + userEmail + ' style="color: #1b9ce2;">' + userEmail + '</a>' : '')
     let sigWebsite = showWebsite ? '<br /><span style="color: #555;">W:</span>&nbsp;<span style="color: #1b9ce2;"><a style="color: #1b9ce2;" title="Lambent Productions" href="https://lambent.tv/" target="_blank" rel="noopener noreferrer" data-auth="NotApplicable" data-linkindex="2">lambent.tv</a></span>' : ''
-    var signatureHTML =  sigStart + sigNameTitle + sigAddress + sigLandline + sigMobile + sigEmail + sigWebsite + '</p>'
+    var signatureHTML = sigStart + sigNameTitle + sigAddress + sigLandline + sigMobile + sigEmail + sigWebsite + '</p>'
 
     return (
         <>
@@ -58,21 +60,50 @@ export function GenerateSignature(props) {
                     <Col md={4} sm={12}>
                         <Form>
                             <Form.Group controlId="signatureForm.FullName">
-                                <Form.Label>Full Name</Form.Label>
-                                <Form.Control type="Text" placeholder="Full Name" name="fullName" onChange={e => setFullName(e.target.value)} value={fullName} />
+                                <FloatingLabel
+                                    controlId="floatingInput"
+                                    className="mb-3"
+                                    label="Full Name"
+                                >
+                                    <Form.Control type="Text" placeholder="Full Name" name="fullName" onChange={e => setFullName(e.target.value)} value={fullName} />
+                                </FloatingLabel>
                             </Form.Group>
                             <Form.Group controlId="signatureForm.JobTitle">
-                                <Form.Label>Job Title</Form.Label>
-                                <Form.Control type="Text" placeholder="Job Title" name="jobTitle" onChange={e => setJobTitle(e.target.value)} value={jobTitle} />
+                                <   FloatingLabel
+                                    controlId="floatingInput"
+                                    className="mb-3"
+                                    label="Job Title"
+                                >
+                                    <Form.Control type="Text" placeholder="Job Title" name="jobTitle" onChange={e => setJobTitle(e.target.value)} value={jobTitle} />
+                                </FloatingLabel>
                             </Form.Group>
                             <Form.Group controlId="signatureForm.Mobile">
-                                <Form.Label>Mobile Number</Form.Label>
-                                <Form.Control type="Tel" placeholder="07123456789" name="mobile" onChange={
-                                    (e) => {
-                                        setMobile(e.target.value)
-                                        setShowMobile(e.target.value.length > 0)
-                                    }
-                                } value={mobile} />
+                                <FloatingLabel
+                                    controlId="floatingInput"
+                                    className="mb-3"
+                                    label="Mobile Number"
+                                >
+                                    <Form.Control type="Tel" placeholder="07123456789" name="mobile" onChange={
+                                        (e) => {
+                                            setMobile(e.target.value)
+                                            setShowMobile(e.target.value.length > 0)
+                                        }
+                                    } value={mobile} />
+                                </FloatingLabel>
+                            </Form.Group>
+                            <Form.Group controlId="signatureForm.Landline">
+                                <FloatingLabel
+                                    controlId="floatingInput"
+                                    className="mb-3"
+                                    label="Landline"
+                                >
+                                    <Form.Control type="Tel" placeholder="01273 789090" name="landline" onChange={
+                                        (e) => {
+                                            setLandline(e.target.value)
+                                            setShowLandline(e.target.value.length > 0)
+                                        }
+                                    } value={landline} />
+                                </FloatingLabel>
                             </Form.Group>
                             <Row>
                                 <Form.Group as={Col} lg={6} md={12} sm={6} xs={12} controlId="signatureForm.ShowLandline">
